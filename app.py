229f5a2c222ebcc7ae90e2dd8af256ec00c6fead
Kamden <kamdenterry2@gmail.com>
2026-09-19 17:38:36 +0200

    Added car model with lockout and owner link

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask 
 from database import db 
 from models import User,Car
-
+from datetime import date 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cars.db"
@@ -9,3 +9,21 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+    kamden = User(name = "Kamden" ,email = "kamdenexample@gmail.com")
+    kamden.set_password("testpassword")
+    db.session.add(kamden)
+    db.session.commit()
+
+    print(kamden.password_hash)
+    print(kamden.check_password("kamden"))
+    print(kamden.is_authenticated)
+
+    car = Car(owner_id = kamden.id, last_service_date = date(2024, 5, 1),make = "hyundai", model = "accent", year = 2019, vin = "02H25K36L25DGK45I")
+    car.set_access_code("test")
+    db.session.add(car)
+    db.session.commit()
+   
+    print(car.qr_token)
+    print(car.failed_attempts)
+    print(car.owner.name)
--- models.py
+++ models.py
@@ -9,7 +9,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique = True , nullable = False)
     phone_number = db.Column(db.String(20))
     password_hash = db.Column(db.String(200),nullable = False)
-
+    cars = db.relationship("Car", backref = "owner")
+    
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
 
@@ -18,12 +19,15 @@ class User(UserMixin, db.Model):
 
 class Car(db.Model):
     id = db.Column(db.Integer,primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
     make = db.Column(db.String(100),nullable = False)
     model = db.Column(db.String(100), nullable = False)
     year = db.Column(db.Integer,nullable = False)
     vin = db.Column(db.String(17), unique =True ,nullable = False)
     last_service_date = db.Column(db.Date(),nullable = False)
     qr_token = db.Column(db.String(32),unique =True, nullable =False,default = lambda: secrets.token_urlsafe(16))
+    failed_attempts = db.Column(db.Integer,nullable = False, default = 0 )
+    locked_until = db.Column(db.DateTime)
     access_code_hash = db.Column(db.String(200), nullable = False)
 
     def set_access_code(self,access_code):
