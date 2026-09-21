abbdec103600453df06160a4a20b296af7c0d253
Kamden <kamdenterry2@gmail.com>
2026-09-21 16:33:40 +0200

    Set up Flask login

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, abort 
+from flask_login import LoginManager
 from database import db 
+import os 
+from dotenv import load_dotenv
 from models import User,Car,ServiceRecord
 
 
 app = Flask(__name__)
+load_dotenv()
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cars.db"
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 with app.app_context():
     db.create_all()
 
-
+@login_manager.user_loader
+def loader(user_id):
+    return db.session.get(User, int(user_id))
 
 
 @app.route("/")
@@ -26,4 +36,5 @@ def show_qr(token):
 
 
 if __name__ == "__main__":
-        app.run(debug = True)
+    app.run(debug = True)
+
